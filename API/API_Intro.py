3b9347b0b471436b8e564f7c70c0b334b263835c
Akhil-Jasson <114808672+Akhil-Jasson@users.noreply.github.com>
2024-01-26 22:20:17 +0530

    Add files via upload

---
+++ API/API_Intro.py
@@ -0,0 +1,12 @@
+# 1xx: Hold On 2xx: Here You Go 3xx:Go Away 4xx:You Screwed Up 5xx: Server screwed Up
+
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (latitude, longitude)
+print(iss_position)
